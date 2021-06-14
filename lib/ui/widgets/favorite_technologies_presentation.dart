999282335c5ad403c307469280a5d787fedb32f6
ssmg <ssmg.sg@gmail.com>
2021-06-14 14:43:14 -0600

    Create a rotating text with a heart animation showing my favorite technologies

---
+++ lib/ui/widgets/favorite_technologies_presentation.dart
@@ -0,0 +1,56 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+/// Creates a text with animated text kit showing my favorite technologies.
+class FavoriteTechnoloiesPresentation extends StatelessWidget {
+  /// Creates a text with animated text kit showing my favorite technologies.
+  const FavoriteTechnoloiesPresentation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Lottie.asset(
+          'assets/heart_animation.json',
+          height: 170,
+          width: 170,
+        ),
+        DefaultTextStyle(
+          style: const TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Inter',
+            color: Colors.black,
+          ),
+          child: AnimatedTextKit(
+            repeatForever: true,
+            pause: const Duration(
+              milliseconds: 0,
+            ),
+            animatedTexts: <AnimatedText>[
+              RotateAnimatedText(
+                'Flutter',
+                duration: const Duration(
+                  milliseconds: 1500,
+                ),
+              ),
+              RotateAnimatedText(
+                'Dart',
+                duration: const Duration(
+                  milliseconds: 1500,
+                ),
+              ),
+              RotateAnimatedText(
+                'Go',
+                duration: const Duration(
+                  milliseconds: 1500,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
