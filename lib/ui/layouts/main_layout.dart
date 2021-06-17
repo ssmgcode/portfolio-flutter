15ce6f07f452c2a7143d41550f9c193e539b1160
ssmg <ssmg.sg@gmail.com>
2021-06-17 07:54:50 -0600

    Create an AppBar

--- lib/ui/layouts/main_layout.dart
+++ lib/ui/layouts/main_layout.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:portfolio/ui/widgets/custom_app_bar.dart';
 
 /// Displays a [Scaffold], with a body with a custom bar navigation, a footer
 /// and a [Expanded] widget for a child.
@@ -16,16 +18,37 @@ class MainLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(
+            left: 8.0,
+          ),
+          child: SvgPicture.asset(
+            'assets/ssmg-logo.svg',
+            height: 35,
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.more_horiz),
+            onPressed: () {},
+          ),
+        ],
+      ),
       body: SafeArea(
-        child: Column(
+        /* child: Column(
           children: <Widget>[
-            // TODO: Implement app navigation bar
-            const Text('App Navigation Bar'),
-            Expanded(
+            SizedBox(
+              width: 800,
               child: child!,
             ),
-            // TODO: Implement footer
             const Text('Footer'),
+          ],
+        ), */
+        child: Column(
+          children: <Widget>[
+            Expanded(child: child!),
           ],
         ),
       ),
