36a6ffc80b0fa6be64e7228dfc4824112b131b91
ssmg <ssmg.sg@gmail.com>
2021-06-17 07:56:34 -0600

    Modify structure

--- lib/ui/views/main_view.dart
+++ lib/ui/views/main_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/ui/widgets/favorite_technologies_presentation.dart';
 import 'package:portfolio/ui/widgets/personal_information.dart';
-import 'package:portfolio/ui/widgets/presentation.dart';
+import 'package:portfolio/ui/widgets/ssmg_poster.dart';
 
 /// Displays the root page of the portfolio.
 class MainView extends StatelessWidget {
@@ -13,8 +12,7 @@ class MainView extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: const <Widget>[
-          Presentation(),
-          FavoriteTechnoloiesPresentation(),
+          SSMGPoster(),
           PersonalInformation(),
         ],
       ),
