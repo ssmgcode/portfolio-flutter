4f5dd0f93a351c175982d1e416b1b7d336dc773b
ssmg <ssmg.sg@gmail.com>
2021-06-21 13:49:37 -0600

    Protect properties

--- lib/config/app_colors.dart
+++ lib/config/app_colors.dart
@@ -2,44 +2,41 @@ import 'package:flutter/material.dart';
 
 /// Contains the colors for the application.
 class AppTheme {
-// This class is not meant to be instantiated or extended; this constructor
+  // This class is not meant to be instantiated or extended; this constructor
   // prevents instantiation and extension.
   AppTheme._();
 
   /// The accent color for the application.
-  static final accentColor = Colors.blue.shade700;
+  static final _accentColor = Colors.blue.shade700;
 
   /// The color of brand.
   static const brandColor = Color.fromRGBO(15, 38, 54, 1);
 
   /// The main font used across the application.
-  static const fontFamily = 'IBMPlexSans';
+  // static const _fontFamily = 'IBMPlexSans';
+  static const _fontFamily = 'Inter';
 
-  static final _lightTheme = ThemeData(
-    fontFamily: fontFamily,
+  /// Light theme of application.
+  static final lightTheme = ThemeData(
+    fontFamily: _fontFamily,
     brightness: Brightness.light,
     primaryColorBrightness: Brightness.light,
     primaryColor: Colors.white,
-    accentColor: accentColor,
-    toggleableActiveColor: accentColor,
+    accentColor: _accentColor,
+    toggleableActiveColor: _accentColor,
     scaffoldBackgroundColor: const Color.fromRGBO(248, 249, 250, 1),
     cardColor: Colors.white,
   );
 
-  static final _darkTheme = ThemeData(
-    fontFamily: fontFamily,
+  /// Dark theme of application.
+  static final darkTheme = ThemeData(
+    fontFamily: _fontFamily,
     brightness: Brightness.dark,
     primaryColorBrightness: Brightness.dark,
     primaryColor: const Color.fromRGBO(36, 37, 38, 1),
-    accentColor: accentColor,
-    toggleableActiveColor: accentColor,
+    accentColor: _accentColor,
+    toggleableActiveColor: _accentColor,
     scaffoldBackgroundColor: const Color.fromRGBO(25, 26, 27, 1),
     cardColor: const Color.fromRGBO(36, 37, 38, 1),
   );
-
-  /// Get the light theme.
-  static ThemeData get lightTheme => _lightTheme;
-
-  /// Get the dark theme.
-  static ThemeData get darkTheme => _darkTheme;
 }
