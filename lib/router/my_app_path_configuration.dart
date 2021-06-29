587e901646419f03c093a38d2bbbee47c0a16756
ssmg <ssmg.sg@gmail.com>
2021-06-28 20:50:34 -0600

    Implements Navigator 2.0 Deletes old stuff relating to Fluto package Change views by pages

---
+++ lib/router/my_app_path_configuration.dart
@@ -0,0 +1,40 @@
+/// Determines what configuration has the application on its path.
+class MyAppPathConfiguration {
+  /// Sets the current application path configuration to home.
+  const MyAppPathConfiguration.home() : _currentPage = 'home';
+
+  /// Sets the current application path configuration to projects.
+  const MyAppPathConfiguration.projects() : _currentPage = 'projects';
+
+  /// Sets the current application path configuration to unknown.
+  const MyAppPathConfiguration.unknown() : _currentPage = '404';
+
+  final String _currentPage;
+
+  /// Returns `true` if the current application path configuration is home.
+  bool get isHome => _currentPage == 'home';
+
+  /// Returns `true` if the current application path configuration is projects.
+  bool get isProjects => _currentPage == 'projects';
+
+  /// Returns `true` if the current application path configuration is unknown.
+  bool get isUnknown => _currentPage == '404';
+
+  /// Parses a url to an instance of [MyAppPathConfiguration] depending of the
+  /// path segments and query parameters.
+  static MyAppPathConfiguration parse(String url) {
+    final uri = Uri.parse(url);
+
+    // path: /
+    if (uri.pathSegments.isEmpty) {
+      return const MyAppPathConfiguration.home();
+    }
+
+    // path: /projects
+    if (uri.pathSegments.first == 'projects') {
+      return const MyAppPathConfiguration.projects();
+    }
+
+    return const MyAppPathConfiguration.unknown();
+  }
+}
---
+++ lib/router/page_manager.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/router/my_app_path_configuration.dart';
+import 'package:portfolio/ui/pages/home_page.dart';
+import 'package:portfolio/ui/pages/projects_page.dart';
+import 'package:provider/provider.dart';
+
+/// The page manager for the router.
+class RouterPageManager extends ChangeNotifier {
+  final List<Page> _pages = [
+    MaterialPage(
+      key: UniqueKey(),
+      name: '/',
+      child: const HomePage(),
+    ),
+  ];
+
+  /// List of pages of the application.
+  List<Page> get pages => List.unmodifiable(_pages);
+
+  /// Current path of application depending of [MyAppPathConfiguration].
+  MyAppPathConfiguration get currentPath =>
+      MyAppPathConfiguration.parse(_pages.last.name!);
+
+  /// Retrieves the instance of RouterPageManager from the widgets tree.
+  static RouterPageManager of(BuildContext context) =>
+      Provider.of<RouterPageManager>(
+        context,
+        listen: false,
+      );
+
+  /// Removes the page which did pop from the pages list.
+  void didPop(RouteSettings settings) {
+    _pages.remove(settings);
+    notifyListeners();
+  }
+
+  /// Add projects page to pages list.
+  void openProjectsPage() {
+    _pages.add(
+      MaterialPage(
+        key: UniqueKey(),
+        name: '/projects',
+        child: const ProjectsPage(),
+      ),
+    );
+    notifyListeners();
+  }
+
+  /// Deletes all pages except home page.
+  void openHomePage() {
+    _pages.removeWhere((page) => page.name != '/');
+  }
+
+  /// Updates internal state of pages when external url changes.
+  Future<void> setNewRoutePath(MyAppPathConfiguration configuration) async {
+    if (configuration.isUnknown) {
+      // TODO: Implement redirection to 404 page.
+    } else if (configuration.isProjects) {
+      openProjectsPage();
+    } else if (configuration.isHome) {
+      openHomePage();
+    }
+  }
+}
