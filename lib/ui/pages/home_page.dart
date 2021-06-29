dea733e9b962a1acb5aad5e4c4d41da95a556a02
ssmg <ssmg.sg@gmail.com>
2021-06-29 14:39:03 -0600

    Add languages and skill sections Delete personal_information file Incorporate personal information directly in home_page

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:portfolio/ui/widgets/app_bar.dart';
-import 'package:portfolio/ui/widgets/personal_information.dart';
+import 'package:portfolio/ui/widgets/custom_card.dart';
+import 'package:portfolio/ui/widgets/favorite_technologies_presentation.dart';
+import 'package:portfolio/ui/widgets/percent_bar/percent_bar.dart';
 import 'package:portfolio/ui/widgets/ssmg_poster.dart';
 
 /// Displays the root view of the portfolio.
@@ -15,12 +19,136 @@ class HomePage extends StatelessWidget {
       body: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
         child: Column(
-          children: const <Widget>[
-            SSMGPoster(),
-            PersonalInformation(),
+          children: <Widget>[
+            const SSMGPoster(),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: 20,
+                horizontal: 10,
+              ),
+              child: SizedBox(
+                width: 1000,
+                child: Column(
+                  children: <Widget>[
+                    const CustomCard(
+                      icon: Icons.person_outline,
+                      title: 'About me',
+                      child: SelectableText(
+                        // ignore: lines_longer_than_80_chars
+                        'I am SSMG, a guatemalan 17 years old guy, software engineer student, full stack developer and Flutter, Dart and Go lover.',
+                      ),
+                    ),
+                    const FittedBox(
+                      child: FavoriteTechnoloiesPresentation(),
+                    ),
+                    CustomCard(
+                      icon: Icons.language_outlined,
+                      title: 'Languages',
+                      child: Column(
+                        children: const <Widget>[
+                          _Skill(
+                            name: 'Spanish - Native',
+                            percent: 1.0,
+                          ),
+                          _Skill(
+                            name: 'English',
+                            percent: 0.75,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    CustomCard(
+                      icon: Icons.hardware_outlined,
+                      title: 'Skills',
+                      child: Column(
+                        children: const <Widget>[
+                          _Skill(
+                            name: 'Dart',
+                            percent: 0.6,
+                          ),
+                          _Skill(
+                            name: 'Flutter',
+                            percent: 0.35,
+                          ),
+                          _Skill(
+                            name: 'Go',
+                            percent: 0.05,
+                          ),
+                          _Skill(
+                            name: 'TypeScript',
+                            percent: 0.75,
+                          ),
+                          _Skill(
+                            name: 'Svelte',
+                            percent: 0.5,
+                          ),
+                          _Skill(
+                            name: 'Git',
+                            percent: 0.30,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
+    );
+  }
+}
+
+class _Skill extends StatelessWidget {
+  const _Skill({
+    Key? key,
+    required this.name,
+    required this.percent,
+  }) : super(key: key);
+
+  final String name;
+  final double percent;
+
+  @override
+  Widget build(BuildContext context) {
+    final random = Random();
+    final randomColor = Color.fromRGBO(
+        random.nextInt(255), random.nextInt(255), random.nextInt(255), 1);
+    return Column(
+      children: <Widget>[
+        Container(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            name,
+            style: TextStyle(
+              color: DefaultTextStyle.of(context).style.color!.withOpacity(0.6),
+              // fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        PercentBar(
+          percent: percent,
+          color: randomColor,
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Container(
+          alignment: Alignment.centerRight,
+          child: Text(
+            '${percent * 100}%',
+            style: Theme.of(context).textTheme.caption,
+          ),
+        ),
+      ],
     );
   }
 }
