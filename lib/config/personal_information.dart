a2ba5936c3724e91310d72548aea4c42d356b7ee
ssmg <ssmg.sg@gmail.com>
2021-06-29 16:23:25 -0600

    Create a personal information class

---
+++ lib/config/personal_information.dart
@@ -0,0 +1,9 @@
+/// Contains all my personal information, such as GitHub profile, country and
+/// others.
+class PersonalInformation {
+  /// This class is not meant to be instantiated or extended; this constructor
+  // prevents instantiation and extension.
+  PersonalInformation._();
+
+  static const githubProfileUrl = 'https://github.com/ssmgcode';
+}
