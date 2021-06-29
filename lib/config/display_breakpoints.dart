ae7c2301532ee5dd6b15742110632b202cdbbcf6
ssmg <ssmg.sg@gmail.com>
2021-06-29 17:16:22 -0600

    Create breakpoints for display sizes

---
+++ lib/config/display_breakpoints.dart
@@ -0,0 +1,9 @@
+/// Contains the most common display breakpoints.
+class DisplayBreakpoints {
+  /// This class is not meant to be instantiated or extended; this constructor
+  // prevents instantiation and extension.
+  DisplayBreakpoints._();
+
+  /// Breakpoint for mobile devices.
+  static const mobile = 768;
+}
