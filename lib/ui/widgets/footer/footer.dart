2b6c1be3cfc3ebb58d779a43cf265d5b8681d365
ssmg <ssmg.sg@gmail.com>
2021-07-04 00:16:45 -0600

    Implement flow to open a footer link

--- lib/ui/widgets/footer/footer.dart
+++ lib/ui/widgets/footer/footer.dart
@@ -15,9 +15,15 @@ class Footer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(50.0),
-      // height: 250,
       width: double.infinity,
-      color: Theme.of(context).cardColor,
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            color: DefaultTextStyle.of(context).style.color!.withOpacity(0.1),
+            width: 5.0,
+          ),
+        ),
+      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
@@ -92,14 +98,29 @@ class _SocialMediaIcon extends StatelessWidget {
         DefaultTextStyle.of(context).style.color!.withOpacity(0.6);
     return GestureDetector(
       onTap: () async {
-        await Clipboard.setData(
-          ClipboardData(
-            text: url,
-          ),
-        );
-        SnackBarService.showInformationSnackBar('URL copied to clipboard');
-        if (await canLaunch(url)) {
-          // await launch(url);
+        final canOpenUrl = await canLaunch(url);
+        if (!canOpenUrl) {
+          await launch(url);
+          return;
+        }
+        try {
+          await Clipboard.setData(
+            ClipboardData(
+              text: url,
+            ),
+          );
+          SnackBarService.showInformationSnackBar(
+            message: 'URL copied to clipboard',
+            action: SnackBarAction(
+              label: 'Dismiss',
+              onPressed: () {},
+            ),
+          );
+        } catch (error) {
+          SnackBarService.showInformationSnackBar(
+            message: url,
+            isColored: false,
+          );
         }
       },
       child: SvgPicture.asset(
