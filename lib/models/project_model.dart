9ce0d2e7caadaeed9737fd537716889b2f1f46c6
ssmg <ssmg.sg@gmail.com>
2021-07-05 15:42:22 -0600

    Create the model for project

---
+++ lib/models/project_model.dart
@@ -0,0 +1,37 @@
+import 'package:portfolio/models/technologies_enum.dart';
+
+/// The model of the projects to present in `ProjectsPage`.
+class Project {
+  /// The model of the projects to present in `ProjectsPage`.
+  const Project({
+    required this.description,
+    required this.name,
+    required this.technologies,
+    this.appleAppStoreUrl,
+    this.googlePlayStoreUrl,
+    this.repositoryUrl,
+    this.webUrl,
+  });
+
+  /// Name of the project.
+  final String name;
+
+  /// Optional description of the project.
+  final String description;
+
+  /// A list of the technologies used in the project.
+  /// Typically, programming languages.
+  final List<Technology> technologies;
+
+  /// Optional url of the repository.
+  final String? repositoryUrl;
+
+  /// Optional url of the web site deployed.
+  final String? webUrl;
+
+  /// Optional url of the application in Google Play Store.
+  final String? googlePlayStoreUrl;
+
+  /// Optional url of the application in Apple AppStore.
+  final String? appleAppStoreUrl;
+}
