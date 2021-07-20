49025e2dd7149f4fbdd20f169ce82eb7b2850d2a
ssmg <ssmg.sg@gmail.com>
2021-07-20 12:29:26 -0600

    Create structure of contact page

--- lib/ui/pages/contact_me_page.dart
+++ lib/ui/pages/contact_me_page.dart
@@ -10,6 +10,101 @@ class ContactMePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(context),
+      body: LayoutBuilder(
+        builder: (_, BoxConstraints boxConstraints) => SingleChildScrollView(
+          physics: const BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics(),
+          ),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: boxConstraints.maxHeight,
+            ),
+            child: IntrinsicHeight(
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 15.0,
+                    ),
+                    child: Text(
+                      'Contact Me',
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                    child: SizedBox(
+                      width: 700,
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 20.0,
+                            horizontal: 15.0,
+                          ),
+                          child: Form(
+                            child: Column(
+                              children: <Widget>[
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Name',
+                                    hintText: 'Name',
+                                    prefixIcon: Icon(Icons.person_outlined),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 15.0,
+                                ),
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Email',
+                                    hintText: 'example@mail.com',
+                                    prefixIcon: Icon(Icons.email_outlined),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 15.0,
+                                ),
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Subject',
+                                    hintText: 'Subject',
+                                    prefixIcon: Icon(Icons.subject_outlined),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 15.0,
+                                ),
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Message',
+                                    hintText: 'Message',
+                                    prefixIcon: Icon(Icons.message_outlined),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20.0,
+                                ),
+                                OutlinedButton(
+                                  onPressed: () {},
+                                  child: Text('Submit'),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
