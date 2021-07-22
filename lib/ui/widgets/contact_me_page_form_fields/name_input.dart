8274b44f0ffaac0edf904890208f69d58c47d100
ssmg <ssmg.sg@gmail.com>
2021-07-22 09:01:51 -0600

    Implement the validator for NameInput

--- lib/ui/widgets/contact_me_page_form_fields/name_input.dart
+++ lib/ui/widgets/contact_me_page_form_fields/name_input.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:portfolio/bloc/contact_me_form_bloc/contact_me_form_bloc.dart';
 
 /// The field where user will enter their name.
 class NameInput extends StatelessWidget {
@@ -13,16 +15,31 @@ class NameInput extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      keyboardType: TextInputType.name,
-      textCapitalization: TextCapitalization.words,
-      textInputAction: TextInputAction.next,
-      decoration: const InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: 'Name',
-        hintText: 'Name',
-        prefixIcon: Icon(Icons.person_outlined),
-      ),
+    final contactMeFormBloc = BlocProvider.of<ContactMeFormBloc>(context);
+    return BlocBuilder<ContactMeFormBloc, ContactMeFormState>(
+      builder: (BuildContext context, ContactMeFormState state) {
+        return TextFormField(
+          initialValue: state.name.value,
+          focusNode: focusNode,
+          keyboardType: TextInputType.name,
+          textCapitalization: TextCapitalization.words,
+          textInputAction: TextInputAction.next,
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            labelText: 'Name',
+            hintText: 'Name',
+            prefixIcon: const Icon(Icons.person_outlined),
+            errorText: state.name.invalid
+                ? 'Name must have at least 2 characters.'
+                : null,
+          ),
+          onChanged: (String value) => contactMeFormBloc.add(
+            NameChanged(
+              name: value,
+            ),
+          ),
+        );
+      },
     );
   }
 }
