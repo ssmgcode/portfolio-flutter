b0757f22f2470963eeac28d9565d066ecd14dda0
ssmg <ssmg.sg@gmail.com>
2021-08-30 14:10:25 -0600

    Move done projects to PersonalInformation class

--- lib/config/personal_information.dart
+++ lib/config/personal_information.dart
@@ -1,7 +1,9 @@
-/// Contains all my personal information, such as Github profile, country and
-/// others.
+import 'package:portfolio/models/project_model.dart';
+
+/// Contains all my personal information, such as Github profile, done projects
+/// and more.
 class PersonalInformation {
-  /// This class is not meant to be instantiated or extended; this constructor
+  // This class is not meant to be instantiated or extended; this constructor
   // prevents instantiation and extension.
   PersonalInformation._();
 
@@ -17,4 +19,31 @@ class PersonalInformation {
 
   /// This project repository.
   static const projectRepositoryUrl = '$githubProfileUrl/portfolio';
+
+  /// All my projects based on the [Project] model.
+  static const List<Project> projects = [
+    Project(
+      name: 'SSMG Code Portfolio',
+      description: '''
+My personal portfolio built on Flutter and available for Web and Android.''',
+      technologies: <Technology>[
+        Technology.dart,
+        Technology.flutter,
+        Technology.go,
+      ],
+      repositoryUrl: PersonalInformation.projectRepositoryUrl,
+      webUrl: 'https://ssmgcode-portfolio.vercel.app',
+    ),
+    Project(
+      name: 'Antojitos del Parque landing page',
+      description: '''
+Landing page to show location, schedule, menu, social media and more information relating to the brand.''',
+      technologies: <Technology>[
+        Technology.svelte,
+      ],
+      repositoryUrl:
+          'https://github.com/ssmgcode/antojitos-del-parque-landing-page',
+      webUrl: 'https://antojitosdelparque.com',
+    ),
+  ];
 }
--- lib/ui/pages/projects_page.dart
+++ lib/ui/pages/projects_page.dart
@@ -1,26 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/config/personal_information.dart';
 import 'package:portfolio/models/project_model.dart';
-import 'package:portfolio/models/technologies_enum.dart';
 import 'package:portfolio/ui/widgets/app_bar.dart';
 import 'package:portfolio/ui/widgets/footer/footer.dart';
 import 'package:portfolio/ui/widgets/project_card/project_card.dart';
-
-/// All my projects based on the [Project] model.
-const List<Project> myProjects = [
-  Project(
-    name: 'SSMG Code Portfolio',
-    description: '''
-My personal portfolio built on Flutter and available for Web and Android.''',
-    technologies: [
-      Technology.dart,
-      Technology.flutter,
-      Technology.go,
-    ],
-    repositoryUrl: PersonalInformation.projectRepositoryUrl,
-    webUrl: 'https://ssmgcode-portfolio.vercel.app',
-  ),
-];
 
 /// Displays the root view of the portfolio.
 class ProjectsPage extends StatelessWidget {
@@ -60,7 +43,7 @@ class ProjectsPage extends StatelessWidget {
                       width: 700,
                       child: Column(
                         children: <Widget>[
-                          for (Project project in myProjects)
+                          for (Project project in PersonalInformation.projects)
                             ProjectCard(
                               project: project,
                             ),
